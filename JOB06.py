65dc24db5c07bfe894dd9dab9981b3a906c83756
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-26 11:52:43 +0100

    JOB06 OK

---
+++ JOB06.py
@@ -0,0 +1,8 @@
+
+N = int(input("choisissez un nombre entier supérieur à 0 : "))
+print(f"La table de multiplication de 7 jusqu'à {N} est de :")
+
+i = 0
+while i < (N + 1):
+    print(f"{i} x 7 = {i * 7}")
+    i = i + 1
