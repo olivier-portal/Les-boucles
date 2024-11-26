c67d3a61f82ae1858667d4f3112a4b2345d81fbc
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-26 10:35:59 +0100

    Les boucles en python

---
+++ JOB01.py
@@ -0,0 +1,25 @@
+# for N in range(0, 21):
+#     print(N)
+
+
+# ----------------JOB01
+i = 0
+
+while i <= 20:
+    print(i)
+    i = i + 1
+
+
+# ----------------JOB02
+i = 0
+
+while i <= 20:
+    print(i)
+    i = i + 2
+
+# ----------------JOB03
+i = 1
+
+while i <= 20:
+    print(i**2)
+    i = i + 1
---
+++ JOB04.py
@@ -0,0 +1,8 @@
+# ----------------JOB04
+for N in range(10):
+    print(f"La table de multiplication de {N + 1}")        
+    for i in range(10):
+        print(i, "x", N + 1, "=", i * N)
+        
+        
+    
